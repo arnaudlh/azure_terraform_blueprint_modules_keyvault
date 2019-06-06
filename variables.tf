b6bc14290a55b9312e843a8e5be6f6e3b44a96a7
Arnaud Lheureux <arnaudlh@users.noreply.github.com>
2019-06-06 15:56:26 +0800

    np

--- variables.tf
+++ variables.tf
@@ -5,11 +5,3 @@ variable "resource_group_name" {
 variable "location" {
   description = "(Required) Define the region where the resources will be created"
 }
-
-variable "name" {
-  description = "(required) name of the KV to be deployed"
-}
-
-variable "prefix" {
-  description = "(required) prefix of the KV to be deployed"
-}
